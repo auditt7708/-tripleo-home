946c573386131f08b5e2460bdad319b4919d09fe
Brendan <brendan.shephard@gmail.com>
2020-05-11 12:02:54 +1000

    Ensure everything is pointing to it's full path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ deploy(){
 		-n /home/stack/templates/network_data.yaml \
 		-e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
 		-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-                -e generated-openstack-tripleo-heat-templates/environments/net-multiple-nics-vlans.yaml \
+                -e /home/stack/templates/generated-openstack-tripleo-heat-templates/environments/net-multiple-nics-vlans.yaml \
 		-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dvr-ha.yaml \
 		-e /home/stack/templates/node-info.yaml \
 		-e /home/stack/templates/network-config.yaml \
@@ -14,7 +14,8 @@ deploy(){
 		-e /home/stack/templates/kernel_args.yaml \
 		-e /home/stack/templates/enable-tls.yaml \
 		-e /home/stack/templates/tls-endpoints-public-ip.yaml \
-		-e /home/stack/templates/inject-trust-anchor.yaml
+		-e /home/stack/templates/inject-trust-anchor.yaml \
+		-e /home/stack/templates/overcommit.yaml
 
 }
 
