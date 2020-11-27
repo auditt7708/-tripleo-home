2ae0ccea296effd8683d935b68b07920fa1ec397
Brendan Shephard <brendan.shephard@gmail.com>
2020-11-27 15:11:24 +1000

    Add latest templates and deploy command

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,10 @@ deploy(){
                 -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
 		-e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
 		-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/net-multiple-nics.yaml \
+                -e ~/templates/network-environment-overrides.yaml \
 		-e /home/stack/templates/network-config.yaml \
 		-e /home/stack/templates/storage_custom.yaml \
 		-e /home/stack/templates/kernel_args.yaml \
-		-e /home/stack/templates/enable-tls.yaml \
-		-e /home/stack/templates/tls-endpoints-public-ip.yaml \
-		-e /home/stack/templates/inject-trust-anchor.yaml \
 		-e /home/stack/templates/overcommit.yaml \
 		-e /home/stack/containers-prepare-parameter.yaml 
 
