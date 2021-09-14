b3c7f893601597da33322f53b43f0af4cc01a0a4
Brendan Shephard <brendan.shephard@gmail.com>
2021-09-14 17:01:53 +1000

    Add latest update script

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ update_prepare(){
                 -r /home/stack/templates/roles_data.yaml \
                 -n /home/stack/templates/network_data_v2.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
-                -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
                 -e /home/stack/templates/octavia-config.yaml \
@@ -19,24 +18,21 @@ update_prepare(){
                 -e /home/stack/templates/tls-endpoints-public-dns.yaml \
                 -e /home/stack/templates/cloudname.yaml \
                 -e /home/stack/templates/inject-trust-anchor.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
                 -e /home/stack/templates/service_tuning.yaml \
                 -e /home/stack/templates/networks-deployed.yaml \
-		-e /home/stack/templates/baremetal-deployed.yaml \
+                -e /home/stack/templates/baremetal-deployed.yaml \
                 -e /home/stack/templates/vips-deployed.yaml \
-		-e /home/stack/templates/vip_mapping.yaml \
-		-e /home/stack/templates/set-deletion-policy.yaml \
+                -e /home/stack/templates/vip_mapping.yaml \
+                -e /home/stack/templates/set-deletion-policy.yaml \
                 -e /home/stack/containers-prepare-parameter.yaml
-	}
+}
 
 update_converge(){
         openstack overcloud update converge --templates --disable-validations --deployed-server \
                 -r /home/stack/templates/roles_data.yaml \
                 -n /home/stack/templates/network_data_v2.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
-                -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
                 -e /home/stack/templates/octavia-config.yaml \
                 -e ~/templates/network-environment-overrides.yaml \
@@ -49,11 +45,12 @@ update_converge(){
                 -e /home/stack/templates/tls-endpoints-public-dns.yaml \
                 -e /home/stack/templates/cloudname.yaml \
                 -e /home/stack/templates/inject-trust-anchor.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
                 -e /home/stack/templates/service_tuning.yaml \
                 -e /home/stack/templates/networks-deployed.yaml \
+                -e /home/stack/templates/baremetal-deployed.yaml \
                 -e /home/stack/templates/vips-deployed.yaml \
-		-e /home/stack/templates/vip_mapping.yaml \
+                -e /home/stack/templates/vip_mapping.yaml \
+                -e /home/stack/templates/set-deletion-policy.yaml \
                 -e /home/stack/containers-prepare-parameter.yaml
 }
 
