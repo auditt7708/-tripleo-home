ea4ddb10072073a68c7a92073350dd3035bf1920
Brendan <brendan.shephard@gmail.com>
2021-09-17 05:57:17 +0000

    Keystone ldap

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ deploy(){
                 -r /home/stack/templates/roles_data.yaml \
                 -n /home/stack/templates/network_data_v2.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
-                -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
                 -e /home/stack/templates/octavia-config.yaml \
@@ -19,13 +18,13 @@ deploy(){
                 -e /home/stack/templates/tls-endpoints-public-dns.yaml \
                 -e /home/stack/templates/cloudname.yaml \
                 -e /home/stack/templates/inject-trust-anchor.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
                 -e /home/stack/templates/service_tuning.yaml \
                 -e /home/stack/templates/networks-deployed.yaml \
                 -e /home/stack/templates/baremetal-deployed.yaml \
                 -e /home/stack/templates/vips-deployed.yaml \
                 -e /home/stack/templates/vip_mapping.yaml \
                 -e /home/stack/templates/set-deletion-policy.yaml \
+		-e /home/stack/templates/bne-home-ldap-config.yaml \
                 -e /home/stack/containers-prepare-parameter.yaml
 }
 
@@ -50,34 +49,41 @@ deploy_stack_only(){
                 -e /home/stack/templates/tls-endpoints-public-dns.yaml \
                 -e /home/stack/templates/cloudname.yaml \
                 -e /home/stack/templates/inject-trust-anchor.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
                 -e /home/stack/templates/service_tuning.yaml \
                 -e /home/stack/templates/networks-deployed.yaml \
                 -e /home/stack/templates/baremetal-deployed.yaml \
                 -e /home/stack/templates/vips-deployed.yaml \
                 -e /home/stack/templates/vip_mapping.yaml \
                 -e /home/stack/templates/set-deletion-policy.yaml \
+		-e /home/stack/templates/bne-home-ldap-config.yaml \
                 -e /home/stack/containers-prepare-parameter.yaml	
 }
 
 deploy_config_download(){
         openstack overcloud deploy --templates --config-download-only --disable-validations --deployed-server \
-                -n /home/stack/templates/network_data.yaml \
+                -r /home/stack/templates/roles_data.yaml \
+                -n /home/stack/templates/network_data_v2.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
-		-e /home/stack/templates/overcloud-baremetal-deployed.yaml \
                 -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dvr-ha.yaml \
-                -e /home/stack/templates/node-info.yaml \
+                -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
+                -e /home/stack/templates/octavia-config.yaml \
+                -e ~/templates/network-environment-overrides.yaml \
                 -e /home/stack/templates/network-config.yaml \
+                -e /home/stack/templates/octavia-config.yaml \
                 -e /home/stack/templates/storage_custom.yaml \
                 -e /home/stack/templates/kernel_args.yaml \
+                -e /home/stack/templates/compute_overcommit.yaml \
                 -e /home/stack/templates/enable-tls.yaml \
-                -e /home/stack/templates/tls-endpoints-public-ip.yaml \
+                -e /home/stack/templates/tls-endpoints-public-dns.yaml \
+                -e /home/stack/templates/cloudname.yaml \
                 -e /home/stack/templates/inject-trust-anchor.yaml \
-		-e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
-                -e /home/stack/templates/overcommit.yaml
-
+                -e /home/stack/templates/service_tuning.yaml \
+                -e /home/stack/templates/networks-deployed.yaml \
+                -e /home/stack/templates/baremetal-deployed.yaml \
+                -e /home/stack/templates/vips-deployed.yaml \
+                -e /home/stack/templates/vip_mapping.yaml \
+                -e /home/stack/templates/set-deletion-policy.yaml \
+                -e /home/stack/containers-prepare-parameter.yaml
 }
 
 $@
