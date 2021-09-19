77db6e9a9061cf9c9c0ab353e35b92d954fb38ba
Brendan Shephard <brendan.shephard@gmail.com>
2021-09-19 11:13:27 +1000

    Update converge update
    Update converge no longer requires env var or
    templates arguments. We can remove them and
    just run the update converge command alone now.

--- update.sh
+++ update.sh
@@ -28,30 +28,7 @@ update_prepare(){
 }
 
 update_converge(){
-        openstack overcloud update converge --templates --disable-validations --deployed-server \
-                -r /home/stack/templates/roles_data.yaml \
-                -n /home/stack/templates/network_data_v2.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-                -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
-                -e /home/stack/templates/octavia-config.yaml \
-                -e ~/templates/network-environment-overrides.yaml \
-                -e /home/stack/templates/network-config.yaml \
-                -e /home/stack/templates/octavia-config.yaml \
-                -e /home/stack/templates/storage_custom.yaml \
-                -e /home/stack/templates/kernel_args.yaml \
-                -e /home/stack/templates/compute_overcommit.yaml \
-                -e /home/stack/templates/enable-tls.yaml \
-                -e /home/stack/templates/tls-endpoints-public-dns.yaml \
-                -e /home/stack/templates/cloudname.yaml \
-                -e /home/stack/templates/inject-trust-anchor.yaml \
-                -e /home/stack/templates/service_tuning.yaml \
-                -e /home/stack/templates/networks-deployed.yaml \
-                -e /home/stack/templates/baremetal-deployed.yaml \
-                -e /home/stack/templates/vips-deployed.yaml \
-                -e /home/stack/templates/vip_mapping.yaml \
-                -e /home/stack/templates/set-deletion-policy.yaml \
-                -e /home/stack/containers-prepare-parameter.yaml
+        openstack overcloud update converge -y
 }
 
 $@
